a8d4181128cbf6f81bb450c791c674b6968d1cf2
Bekurt <attilio.dinunno@gmail.com>
2023-11-21 16:51:59 +0100

    Project setup

---
+++ app/redux/slices/boilerSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const boilerSlice = createSlice({
+  name: "boiler",
+  initialState: null,
+  reducers: {
+
+  }
+})
+
+export const boilerReducer = boilerSlice.reducer
